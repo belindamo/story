13a9413218d5c154615708f6c426cd5190511fa3
justanexperiment <justanexperimentpark@gmail.com>
2024-06-28 23:47:03 -0700

    Add electon forge publish dependency

--- forge.config.ts
+++ forge.config.ts
@@ -47,6 +47,18 @@ const config: ForgeConfig = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'justanexperiment',
+          name: 'story'
+        },
+        prerelease: true
+      },
+    }
+  ]
 };
 
 export default config;
